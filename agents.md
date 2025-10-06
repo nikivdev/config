ff039915517f5c5d2910ae2ab96f7b45c6974b2e
Nikita <github@nikiv.dev>
2025-10-06 14:51:55 +0300

    .

---
+++ agents.md
@@ -0,0 +1,1 @@
+- when starting with `sh`, create shell script in sh/ folder
--- fish/config.fish
+++ fish/config.fish
@@ -8,46 +8,73 @@ set -x NIX_SSL_CERT_FILE /etc/ssl/certs/ca-certificates.crt # needed for flox/ni
 # direnv hook fish | source # direnv (cd into folder, will get the `.env` etc.)
 # set -x DIRENV_LOG_FORMAT "" # hide direnv unloading messages
 
-fish_add_path $HOME/.flox/run/aarch64-darwin.default.dev/bin
-fish_add_path ~/go/bin
-fish_add_path /opt/homebrew/bin
-fish_add_path "$HOME/.bun/bin"
-fish_add_path "$HOME/.cargo/bin"
+if test -d $HOME/.flox/run/aarch64-darwin.default.dev/bin
+    fish_add_path --global --prepend $HOME/.flox/run/aarch64-darwin.default.dev/bin
+end
+if test -d $HOME/go/bin
+    fish_add_path --global --prepend $HOME/go/bin
+end
+if test -d /opt/homebrew/bin
+    fish_add_path --global --prepend /opt/homebrew/bin
+end
+if test -d "$HOME/.bun/bin"
+    fish_add_path --global --prepend $HOME/.bun/bin
+end
+if test -d "$HOME/.cargo/bin"
+    fish_add_path --global --prepend $HOME/.cargo/bin
+end
 
-set -gx PATH /Users/nikiv/.local/share/solana/install/active_release/bin $PATH # solana https://solana.com/docs/intro/installation
+# solana https://solana.com/docs/intro/installation
+if test -d /Users/nikiv/.local/share/solana/install/active_release/bin
+    fish_add_path --global --prepend /Users/nikiv/.local/share/solana/install/active_release/bin
+end
 
 # Added by Windsurf
-fish_add_path /Users/nikiv/.codeium/windsurf/bin
+if test -d /Users/nikiv/.codeium/windsurf/bin
+    fish_add_path --global --prepend /Users/nikiv/.codeium/windsurf/bin
+end
 
 # Added by LM Studio CLI (lms)
-set -gx PATH $PATH /Users/nikiv/.cache/lm-studio/bin
+if test -d /Users/nikiv/.cache/lm-studio/bin
+    fish_add_path --global --append /Users/nikiv/.cache/lm-studio/bin
+end
 
 # fnm
 # TODO: looks ugly and seems wrong to commit this in config.fish, fix it
-set -gx PATH "/Users/nikiv/.local/state/fnm_multishells/6982_1733503676213/bin" $PATH;
-set -gx FNM_MULTISHELL_PATH "/Users/nikiv/.local/state/fnm_multishells/6982_1733503676213";
-set -gx FNM_VERSION_FILE_STRATEGY "local";
-set -gx FNM_DIR "/Users/nikiv/Library/Application Support/fnm";
-set -gx FNM_LOGLEVEL "info";
-set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist";
-set -gx FNM_COREPACK_ENABLED "false";
-set -gx FNM_RESOLVE_ENGINES "true";
-set -gx FNM_ARCH "arm64";
+set -gx FNM_MULTISHELL_PATH "/Users/nikiv/.local/state/fnm_multishells/6982_1733503676213"
+set -gx FNM_VERSION_FILE_STRATEGY "local"
+set -gx FNM_DIR "/Users/nikiv/Library/Application Support/fnm"
+set -gx FNM_LOGLEVEL "info"
+set -gx FNM_NODE_DIST_MIRROR "https://nodejs.org/dist"
+set -gx FNM_COREPACK_ENABLED "false"
+set -gx FNM_RESOLVE_ENGINES "true"
+set -gx FNM_ARCH "arm64"
+
+set -l _fnm_bin "$FNM_MULTISHELL_PATH/bin"
+if test -d $_fnm_bin
+    fish_add_path --global --prepend $_fnm_bin
+end
 
 # moonbit
-fish_add_path "$HOME/.moon/bin"
+if test -d "$HOME/.moon/bin"
+    fish_add_path --global --prepend $HOME/.moon/bin
+end
 
 atuin init fish  --disable-up-arrow | source
 
 jumpy completions fish | source
 
 # for pg_dump
-fish_add_path /opt/homebrew/opt/libpq/bin
+if test -d /opt/homebrew/opt/libpq/bin
+    fish_add_path --global --prepend /opt/homebrew/opt/libpq/bin
+end
 
-fish_add_path /Users/nikiv/.modular/bin
+if test -d /Users/nikiv/.modular/bin
+    fish_add_path --global --prepend /Users/nikiv/.modular/bin
+end
 
 if test -e "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
-    fish_add_path "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin"
+    fish_add_path --global --prepend "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin"
     test -f "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc" && source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
     test -f "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc" && source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.fish.inc"
 end
@@ -56,10 +83,15 @@ source ~/.orbstack/shell/init2.fish 2>/dev/null || true
 
 # pnpm
 set -gx PNPM_HOME "/Users/nikiv/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+if test -d $PNPM_HOME
+    fish_add_path --global --prepend $PNPM_HOME
 end
 # pnpm end
-fish_add_path /Users/nikiv/.pixi/bin
+if test -d /Users/nikiv/.pixi/bin
+    fish_add_path --global --prepend /Users/nikiv/.pixi/bin
+end
+if test -d $HOME/src/config/sh
+    fish_add_path --global --prepend $HOME/src/config/sh
+end
 # Added by zv setup
 source "/Users/nikiv/.zv/env.fish"
